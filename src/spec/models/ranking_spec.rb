4ca54be732e0e2b14e15758ac05ce719d18a285e
santiago <santiagovazquez010@gmail.com>
2023-09-22 17:53:40 -0300

    test de ranking

---
+++ src/spec/models/ranking_spec.rb
@@ -0,0 +1,30 @@
+require 'sinatra/activerecord'
+require_relative '../../models/init.rb'
+
+describe Ranking do
+ 
+  it "is invalid without a user_id" do 
+    user = User.create(name: "Veget", email:"1110000010@mail.com", password:"ppaa00034")
+    ranking = Ranking.new(score:user.total_score)
+    expect(ranking.valid?).to eq(false)
+  end
+
+  it "is invalid whitout a score" do
+    user = User.create(name: "V", email:"10000010@mail.com", password:"pa00034")
+    ranking = Ranking.new(user_id:user.id)
+    expect(ranking.valid?).to eq(false)
+  end
+  
+  it "is valid whit a user_id and score" do
+    user = User.create(name: "Veget", email:"1110000010@mail.com", password:"ppaa00034")
+    ranking = Ranking.new(score: user.total_score,user_id: user.id)
+    expect(ranking.valid?).to eq(true)
+  end
+
+  it "It is invalid if the score is not a number" do
+    user = User.create(name: "Vegtte", email:"1110000010@mail.com", password:"ppaa00034")
+    ranking = Ranking.new(score: "H",user_id: user.id)
+    expect(ranking.valid?).to eq(false)
+  end
+
+end
